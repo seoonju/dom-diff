b0feb0655a9c82ca724ee358898b0320d0859d84
seoonju <tjswn9477@gmail.com>
2025-07-24 11:54:07 +0900

    [Autofic] 2 malicious code detected!!

--- src/diff/worker.js
+++ src/diff/worker.js
@@ -2,6 +2,9 @@ import diff from './main';
 
 // eslint-disable-next-line no-undef
 self.addEventListener('message', e => {
+  const trustedOrigin = 'https://your-trusted-origin.com'; // Replace with your trusted origin
+  if (e.origin !== trustedOrigin) return; // Check the origin of the message
+
   const instructions = diff.apply(null, e.data);
   // eslint-disable-next-line no-undef
   self.postMessage(instructions);
